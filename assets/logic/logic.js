ea29c8ed4f6848a127c2db53d22c6b28dd3b2439
Tellis <teellis20@gmail.com>
2019-09-25 23:53:03 -0700

    whole new portfolio revamp

---
+++ assets/logic/logic.js
@@ -0,0 +1,16 @@
+// pushpin call
+
+// $('.pushpin').each(function() {
+//     var $this = $(this);
+//     var $target = $('#' + $(this).attr('data-target'));
+//     $this.pushpin({
+//       top: $target.offset().top,
+//       bottom: $target.offset().top + $target.outerHeight() - $this.height()
+//     });
+//   });
+
+     
+//   collapsible call
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
